01031bbed7908410b77607061c3df32a5252a5c8
Wzdrahman <wzd.rahman7@gmail.com>
2021-01-20 18:31:52 +0600

    delete posts function created

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ import { ui } from './ui';
 
 // Get post on Dom Load
 document.addEventListener('DOMContentLoaded', getPosts);
+
+// Listen for add post
 document.querySelector('.post-submit').addEventListener('click', addPost);
+
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
 
 // Get Posts
 function getPosts () {
@@ -34,5 +39,21 @@ function addPost () {
     .catch(err => console.log(err));
 }
 
+function deletePost(e) {
+  http.delete();
 
+  e.preventDefault();
+
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if (confirm('Are you Sure?')) {
+      http.delete(`http://localhost:3000/post/${id}`)
+        .then(data => {
+          ui.showAlert('Post Removed', 'alert alert-success')
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+}
 
